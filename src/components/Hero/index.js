a028397bebb30058702256a9a2e1791cdf9b3654
Lehady <lxs4287@rit.edu>
2021-02-07 22:07:26 +0100

    Created hero section of the website

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Video from '../../assets/videos/video.mp4';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroTitle, HeroText, HeroBtnWrapper, ArrowForward, ArrowRight } from "./HeroElements";
+import { Button } from "../ButtonElement";
 
 const Hero = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer id="home">
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
-            </HeroBg>            
+            </HeroBg>
+            <HeroContent>
+                <HeroTitle>Virtual Banking Made Easy</HeroTitle>
+                <HeroText>
+                    Sign up for a new account today and receive $250
+                    in credit towards your next payment.
+                </HeroText>
+                <HeroBtnWrapper>
+                    <Button to="signup" 
+                    onMouseEnter={onHover} 
+                    onMouseLeave={onHover}
+                    primary='true'
+                    dark='true'
+                    >
+                        Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>            
         </HeroContainer>
     )
 }
